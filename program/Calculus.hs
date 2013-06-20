1b1630c8cff62b995ff1607eab7511c5a9561c2e
Shengyi Wang <txyyss@gmail.com>
2013-06-21 01:10:39 +0800

    Use monad transformer ReaderT.

--- program/Calculus.hs
+++ program/Calculus.hs
@@ -3,24 +3,28 @@ module Calculus where
 import Data.Maybe (fromJust)
 import Control.Monad.Identity
 import Control.Monad.Error
+import Control.Monad.Reader
 
-data ReducibleArgs = ReducibleArgs {maxSteps :: Int -> Int}
+data ReducibleSettings = ReducibleSettings {maxSteps :: Int -> Int}
 
-stdReducibleArgs = ReducibleArgs {maxSteps = (* 10)}
+stdReducibleSettings = ReducibleSettings {maxSteps = (* 10)}
 
-type Eval a = ErrorT String Identity a
+type Eval a = ReaderT ReducibleSettings (ErrorT String Identity) a
 
 class Eq a => Reducible a where
   loReduce :: a -> Maybe a
 
   lgh :: a -> Int
 
-  evalWith :: ReducibleArgs -> a -> Eval a
-  evalWith args x
+  evalWith :: a -> (Int -> Int) -> Eval a
+  evalWith x stepFun
     | length trace < steps = return $ fromJust $ last trace
     | otherwise = throwError "Can't be reduced!"
-    where steps = maxSteps args $ lgh x
+    where steps = stepFun $ lgh x
           trace = take steps . takeWhile (/=Nothing) . iterate (>>= loReduce) $ Just x
 
+  runEval :: Eval a -> ReducibleSettings -> Either String a
+  runEval ev = runIdentity . runErrorT . runReaderT ev
+
   eval :: a -> Either String a
-  eval = runIdentity . runErrorT . evalWith stdReducibleArgs
+  eval x = runEval ((asks maxSteps) >>= (evalWith x)) stdReducibleSettings
